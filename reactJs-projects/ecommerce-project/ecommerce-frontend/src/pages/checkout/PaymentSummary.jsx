d09af450fe89bf591dd29201de2452d3ade3dbf5
Dharm Patel <pdharm774@gmail.com>
2025-11-10 02:26:12 +0530

    Created Payment summary test with test logic

--- reactJs-projects/ecommerce-project/ecommerce-frontend/src/pages/checkout/PaymentSummary.jsx
+++ reactJs-projects/ecommerce-project/ecommerce-frontend/src/pages/checkout/PaymentSummary.jsx
@@ -15,31 +15,31 @@ const PaymentSummary = ({paymentSummary, getCartData}) => {
     <div className="payment-summary">
             <div className="payment-summary-title">Payment Summary</div>
 
-            <div className="payment-summary-row">
+            <div data-testId="payment-summary-product-cost" className="payment-summary-row">
               <div>Items ({paymentSummary.totalItems}):</div>
               <div className="payment-summary-money">{formatMoney(paymentSummary.productCostCents)}</div>
             </div>
 
-            <div className="payment-summary-row">
+            <div data-testId="payment-summary-shipping-cost" className="payment-summary-row">
               <div>Shipping &amp; handling:</div>
               <div className="payment-summary-money">
                 {formatMoney(paymentSummary.shippingCostCents)}
               </div>
             </div>
 
-            <div className="payment-summary-row subtotal-row">
+            <div data-testId="payment-summary-total-before-tax" className="payment-summary-row subtotal-row">
               <div>Total before tax:</div>
               <div className="payment-summary-money">
                 {formatMoney(paymentSummary.totalCostBeforeTaxCents)}
               </div>
             </div>
 
-            <div className="payment-summary-row">
+            <div data-testId="payment-summary-tax"  className="payment-summary-row">
               <div>Estimated tax (10%):</div>
               <div className="payment-summary-money">{formatMoney(paymentSummary.taxCents)}</div>
             </div>
 
-            <div className="payment-summary-row total-row">
+            <div data-testId="payment-summary-total" className="payment-summary-row total-row">
               <div>Order total:</div>
               <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostCents)}</div>
             </div>
