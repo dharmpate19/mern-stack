b46cb84cec1fdc98bea0a3d35dab061d234a5f52
Dharm Patel <pdharm774@gmail.com>
2025-11-18 00:51:31 +0530

    update url in vite config to deploy

--- reactJs-projects/ecommerce-project/ecommerce-frontend/vite.config.js
+++ reactJs-projects/ecommerce-project/ecommerce-frontend/vite.config.js
@@ -7,10 +7,14 @@ export default defineConfig({
   server : {
     proxy : {
       '/api' : {
-        target : 'http://localhost:3000'
+        target : 'https://ecommerce-5tn4.onrender.com',
+        changeOrigin: true,
+        secure: false,
         }, 
         '/images' : {
-        target : 'http://localhost:3000'
+        target : 'https://ecommerce-5tn4.onrender.com',
+        changeOrigin: true,
+        secure: false,
         }, 
       }
   }
